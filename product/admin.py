5585833399be0181229b618c0bb2c80eddc023cc
Fan Wang <nimadorostkar97@gmail.com>
2022-11-30 12:43:57 +0330

    commit

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
+from import_export.admin import ImportExportModelAdmin, ImportExportMixin
 
-# Register your models here.
+class CategoryAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name',]
+admin.site.register(models.Category, CategoryAdmin)
+
+
+class TypeAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name',]
+admin.site.register(models.Type, TypeAdmin)
+
+
+class ProductAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('img','name','barcode')
+    search_fields = ['name','barcode']
+    list_filter = ('category','type')
+    raw_id_fields = ['category','type']
+admin.site.register(models.Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.html import format_html
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    def __str__(self):
+        return str(self.name)
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    def __str__(self):
+        return str(self.name)
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    barcode = models.CharField(max_length=256, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    image = models.ImageField(default='products/default.png', upload_to='products')
+    def __str__(self):
+        return str(self.name)
+    def img(self):
+        return format_html("<img style='width:30px;border-radius:50%;' src='{}'>".format(self.image.url))
